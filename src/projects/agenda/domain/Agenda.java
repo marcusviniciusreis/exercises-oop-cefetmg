0f760802112e932744ee812aa8f669b36bd6a4d8
Marcus Vinícius <marcus21.dev@gmail.com>
2026-04-17 21:28:44 -0300

    feat: adiciona classe para busca de contatos
    Responsável por realizar buscas de contatos por critérios como nome.

--- src/projects/agenda/domain/Agenda.java
+++ src/projects/agenda/domain/Agenda.java
@@ -10,4 +10,20 @@ public class Agenda {
         contatos.put(contato.getNome(),contato);
     }
 
+
+    public void pesquisarContato(String nome){
+        System.out.println("---------PESQUISANDO CONTATO---------");
+        Contato aux = contatos.get(nome);
+        if (aux!=null){
+            System.out.println("Nome: "+aux.getNome());
+            System.out.println("Endereco: "+aux.getEndereco());
+            System.out.println("Telefone: "+aux.getTelefone());
+            System.out.println("Email: "+aux.getEmail());
+        }else {
+            System.out.println("Contato não encontrado!");
+        }
+    }
+
+
+
 }
