66a6ebba2fd0a20a3ffc6a8b9de78502b76b007d
Marcus Vinícius <marcus21.dev@gmail.com>
2026-04-17 21:30:08 -0300

    feat: adiciona método para alterar contato
    Permite atualizar os dados de um contato existente.

--- src/projects/agenda/domain/Agenda.java
+++ src/projects/agenda/domain/Agenda.java
@@ -24,6 +24,45 @@ public class Agenda {
         }
     }
 
+    public void alterarNome(String nome, String novoNome){
+        if(contatos.containsKey(nome)){
+            Contato contato = contatos.get(nome);
+            contatos.remove(nome);
+            contatos.put(novoNome,contato);
+        }
+    }
+    public void alterarEndereco(String nome,String endereco){
+        if(contatos.containsKey(nome)){
+            Contato aux = contatos.get(nome);
+            aux.setEndereco(endereco);
+            contatos.put(aux.getNome(),aux);
+        }
+    }
+    public void alterarNumero(String nome,String numero){
+        if(contatos.containsKey(nome)){
+            Contato aux = contatos.get(nome);
+            aux.setEndereco(numero);
+            contatos.put(aux.getNome(),aux);
+        }
+    }
+    public void alterarEmail(String nome, String email){
+        if(contatos.containsKey(nome)){
+            Contato aux = contatos.get(nome);
+            aux.setEndereco(email);
+            contatos.put(aux.getNome(),aux);
+        }
+    }
+
+    public void listarContatos(){
+        for(Contato aux : contatos.values()){
+            System.out.println("------------------------");
+            System.out.println("Nome: "+aux.getNome());
+            System.out.println("Endereco: "+aux.getEndereco());
+            System.out.println("Telefone: "+aux.getTelefone());
+            System.out.println("Email: "+aux.getEmail());
+        }
+    }
+
 
 
 }
